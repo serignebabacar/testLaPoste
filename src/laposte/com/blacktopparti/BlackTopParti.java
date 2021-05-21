1115bb3671e92ed868fe45c7759332a8fd3e3e2b
serigne babacar diop <bdiop68@gmail.com>
2021-05-21 20:56:29 +0200

    update

--- src/laposte/com/blacktopparti/BlackTopParti.java
+++ src/laposte/com/blacktopparti/BlackTopParti.java
@@ -10,9 +10,11 @@ import org.openqa.selenium.interactions.Actions;
 import laposte.com.communs.Communs;
 
 public class BlackTopParti {
+	private String linkFindStore = "https://jsapps.c4a0pho0ft-laposteaz1-s1-public.model-t.cc.commerce.ondemand.com/store-finder";
+	private String xpathFindStore = "//a[@href='/store-finder']";
 
 	public void testBrands(WebDriver driver) {
-		driver.get("https://jsapps.c4a0pho0ft-laposteaz1-s1-public.model-t.cc.commerce.ondemand.com/");
+		driver.get(Communs.URL);
 		driver.findElements(By.xpath("//cx-icon[@class='cx-icon fas fa-angle-down']")).get(0).click();
 		Actions builder = new Actions(driver);
 		moveToElement(driver, builder, "//div/nav/cx-generic-link/a[@role='link']", 0, 10);
@@ -37,9 +39,8 @@ public class BlackTopParti {
 	public void testFindStore(WebDriver driver) {
 		driver.manage().window().maximize();
 		driver.get(Communs.URL);
-		driver.findElement(By.xpath("//a[@href='/store-finder']")).click();
-		System.out.println("https://jsapps.c4a0pho0ft-laposteaz1-s1-public.model-t.cc.commerce.ondemand.com/store-finder"
-				.equals(driver.getCurrentUrl()));
+		driver.findElement(By.xpath(xpathFindStore)).click();
+		System.out.println(linkFindStore.equals(driver.getCurrentUrl()));
 		driver.navigate().back();
 	}
 }
