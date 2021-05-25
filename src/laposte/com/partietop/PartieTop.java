f162b5e6b8bd03a0c086b0f5921c84988a523060
serigne babacar diop <bdiop68@gmail.com>
2021-05-25 16:48:08 +0200

    suppression d'une variable inutulisée

--- src/laposte/com/partietop/PartieTop.java
+++ src/laposte/com/partietop/PartieTop.java
@@ -10,11 +10,11 @@ import org.openqa.selenium.interactions.Actions;
 import laposte.com.communs.Communs;
 
 public class PartieTop {
-	private String urlFindStore = "https://jsapps.c4a0pho0ft-laposteaz1-s1-public.model-t.cc.commerce.ondemand.com/store-finder";
 	private String xpathLinkFindStore = "//a[@href='/store-finder']";
-	private String xpathLinkBrands="//cx-icon[@class='cx-icon fas fa-angle-down']";
+	private String xpathLinkBrands = "//cx-icon[@class='cx-icon fas fa-angle-down']";
 	private String xptathLink = "//div/nav/cx-generic-link/a[@role='link']";
 	private String xpathLinkLogin = "//a[@href='/login']";
+
 	public void testBrands(WebDriver driver) {
 		driver.get(Communs.URL);
 		Communs.click(driver.findElements(By.xpath(xpathLinkBrands)).get(0));
@@ -22,7 +22,7 @@ public class PartieTop {
 		moveToElement(driver, builder, xptathLink, 0, 10);
 	}
 
-	private  void moveToElement(WebDriver driver, Actions builder, String xptath, int indice, int length) {
+	private void moveToElement(WebDriver driver, Actions builder, String xptath, int indice, int length) {
 		List<WebElement> elements = driver.findElements(By.xpath(xptath));
 		for (int i = indice; i < length; i++) {
 			WebElement webElement = elements.get(i);
@@ -30,18 +30,17 @@ public class PartieTop {
 		}
 	}
 
-
 	public void testLinkFindStore(WebDriver driver) throws InterruptedException {
-		 Communs.click(Communs.find(driver,By.xpath(xpathLinkFindStore)));
-		//driver.findElement(By.xpath(xpathLinkFindStore)).click();
+		Communs.click(Communs.find(driver, By.xpath(xpathLinkFindStore)));
+		// driver.findElement(By.xpath(xpathLinkFindStore)).click();
 		Thread.sleep(2000);
-		 System.out.println(driver.getTitle().equals("StoreFinder"));
+		System.out.println(driver.getTitle().equals("StoreFinder"));
 		driver.navigate().back();
 	}
 
 	public void testLogin(WebDriver driver) {
-		Communs.click(Communs.find(driver, By.xpath(xpathLinkLogin)));		
-		//driver.findElement(By.xpath(xpathLinkLogin)).click();
+		Communs.click(Communs.find(driver, By.xpath(xpathLinkLogin)));
+		// driver.findElement(By.xpath(xpathLinkLogin)).click();
 		System.out.println(driver.getTitle().equals("Se connecter à La Poste"));
 		driver.navigate().back();
 	}
