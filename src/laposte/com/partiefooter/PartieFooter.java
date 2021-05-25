a4c85cd1337a53d11be4d2fd2bfaaadf4960e748
serigne babacar diop <bdiop68@gmail.com>
2021-05-25 17:32:15 +0200

    correction erreur resultat test

--- src/laposte/com/partiefooter/PartieFooter.java
+++ src/laposte/com/partiefooter/PartieFooter.java
@@ -11,6 +11,6 @@ public class PartieFooter {
 
 	public void testFooter(WebDriver driver) {
 		System.out.println(Communs.isEqualText(driver.findElement(By.xpath(xpathLabelSapCommerceCloud)),
-				"SAP COMMERC E CLOUD"));
+				"SAP COMMERCE CLOUD"));
 	}
 }
